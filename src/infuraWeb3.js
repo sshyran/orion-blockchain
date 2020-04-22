d4344f4fc8635f9f5eed4ffd9f432e5d6e14a52f
koloale <koloale@gmail.com>
2020-04-23 00:29:47 +0300

    Improved WS

--- src/infuraWeb3.js
+++ src/infuraWeb3.js
@@ -14,7 +14,7 @@ function refreshProvider(web3Obj, providerUrl) {
 
             if (retries > 5) {
                 console.log(`Max retries of 5 exceeding: ${retries} times tried`);
-                return setTimeout(refreshProvider, 5000)
+                return setTimeout(() => refreshProvider(web3Obj, providerUrl), 5000);
             }
         } else {
             console.log(`Reconnecting web3 provider ${providerUrl}`);
@@ -26,16 +26,49 @@ function refreshProvider(web3Obj, providerUrl) {
 
     const provider = new Web3.providers.WebsocketProvider(providerUrl);
 
-    provider.on('end', () => retry());
-    provider.on('error', () => retry());
+    provider.on("connect", () => {
+        console.log("*** WebSocket Connected ***")
+        watchBlocks(web3Obj);
+    })
+    provider.on("error", (e) => {
+        console.log("*** WebSocket Error ***:\n", e)
+        retry(e);
+    })
+    provider.on("end", (e) => {
+        console.log("*** WebSocket Ended ***")
+        setTimeout(() => refreshProvider(web3Obj, providerUrl), 5000);
+    })
+    provider.on("close", (e) => {
+        console.log("*** WebSocket Closed ***")
+        refreshProvider(web3Obj, providerUrl);
+    })
+    provider.on("timeout", (e) => {
+        console.log("*** WebSocket Timeout ***")
+        retry(e);
+    })
+    provider.on("exit", (e) => {
+        console.log("*** WebSocket Exit ***")
+        refreshProvider(web3Obj, providerUrl);
+    })
 
     web3Obj.setProvider(provider);
-
     console.log('New Web3 provider initiated');
 
     return provider
 }
 
+function watchBlocks(web3Socket) {
+    let timerId = null;
+    const subscription = web3Socket.eth.subscribe('newBlockHeaders')
+        .on('data', (blockHeader) => {
+            if (timerId) clearTimeout(timerId);
+            timerId = setTimeout(() => {
+                refreshProvider(web3Socket, 'wss://ropsten.infura.io/ws/v3/e7e50056370b47e0b71bdbc746887727');
+            }, 60000)
+        }).on('error', (error) => {
+            console.error("Error on newBlockHeaders:", error);
+        });
+}
 
 refreshProvider(web3, 'wss://ropsten.infura.io/ws/v3/e7e50056370b47e0b71bdbc746887727');
 
